d1916dc2d1c1a24e6e5d22d33d8a8a050ea46e93
Jim Do HLF <89876688+JimDoHLF@users.noreply.github.com>
2024-04-01 15:24:32 -0400

    Clean up code

--- Code/Datanalayzer.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-# Note that this will work with Python3
-def unpickle(file):
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo)
-    return dict
-
--- Code/KeyDecrypt.py
+++ Code/KeyDecrypt.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 
-def getKey():
+def getKey32():
     keyFile = open("key.txt", "r")
     keyStr = keyFile.read()
     key = np.ndarray(shape=(4096,4096), dtype=np.float32)  # Maybe change this to double?
@@ -11,13 +11,26 @@ def getKey():
         key[i][temp] = 1
     return torch.from_numpy(key)
 
+def getKey64():
+    keyFile = open("key.txt", "r")
+    keyStr = keyFile.read()
+    key = np.ndarray(shape=(4096,4096), dtype=np.float64)  # Maybe change this to double?
+    for i in range (4096):
+        # Convert from hex to number
+        temp = int(keyStr[i * 3 : i * 3 + 3], 16)
+        key[i][temp] = 1
+    return torch.from_numpy(key)
+
 def encryptKey(weight, bias):
-    key = torch.transpose(getKey(), dim0=0, dim1=1)
-    w = torch.matmul(weight, key)
-    b = torch.matmul(weight, key)
+    key32 = torch.transpose(getKey32(), dim0=0, dim1=1)
+    key64 = torch.transpose(getKey64(), dim0=0, dim1=1)
+    print(weight.dtype)
+    print(bias.dtype)
+    w = torch.matmul(weight, key32)
+    b = torch.matmul(bias, key32)
     return w, b
 
 def decryptKey(output):
-    key = getKey()
+    key = getKey32()
     o = torch.matmul(output, key)
     return o
